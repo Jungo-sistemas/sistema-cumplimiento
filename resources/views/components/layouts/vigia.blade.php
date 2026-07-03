d4c36ccdd924fbada150af77d6129f5165dbca01
Bastian4007 <dev2.int@vigia.com.mx>
2026-07-03 09:48:50 -0600

    feat(processes): add annexes column and streamline regulations table

--- resources/views/components/layouts/vigia.blade.php
+++ resources/views/components/layouts/vigia.blade.php
@@ -33,6 +33,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ $title ? $title . ' · Vigia' : 'Vigia' }}</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
